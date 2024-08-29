86b2389bc67b654491b6a6751d2bf8bb87c018fd
OdilovAslanbek <odilovaslanbek84@gmail.com>
2024-08-29 09:52:53 +0500

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+     sans: ["Roboto"],
+     inter: ["Inter"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
